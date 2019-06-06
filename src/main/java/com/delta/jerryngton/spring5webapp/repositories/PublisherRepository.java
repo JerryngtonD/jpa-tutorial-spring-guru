2d0e7d0c065aa5085d90e5739be26cf8e49a4aa6
jerryngton <jerryngtond@gmail.com>
2019-06-06 20:57:05 +0300

    created Publisher CRUD repository

---
+++ src/main/java/com/delta/jerryngton/spring5webapp/repositories/PublisherRepository.java
@@ -0,0 +1,8 @@
+package com.delta.jerryngton.spring5webapp.repositories;
+
+import com.delta.jerryngton.spring5webapp.model.Publisher;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PublisherRepository extends CrudRepository<Publisher, Long> {
+
+}
